c29d238c91aa1035e26e647198af22afe0991405
codyStritz <74121962+codyStritz@users.noreply.github.com>
2022-03-11 12:15:04 -0700

    cpa2 getTemp, started printActivity

--- cpa2/comp_assign_part_2.cpp
+++ cpa2/comp_assign_part_2.cpp
@@ -7,5 +7,26 @@
 
 #include <iostream> 
 #include <iomanip> 
-#include <array>
 using namespace std;
+
+// Gets temperature in (F) from user and saves it to temp reference
+void getTemp(int& temp) {
+  cout << "Please enter the temperature (F): ";
+  cin >> setw(3) >> temp;
+}
+
+void printActivity(int temp) {
+  string activity = "No Activity - Sending Everyone Home";
+  if (temp > 85) activity = "Swimming";
+  // else if (temp )
+
+}
+
+int main()
+{
+  // TODO: Implement main()
+  int temp;
+  getTemp(temp);
+  cout << temp << endl;
+  return 0;
+}
