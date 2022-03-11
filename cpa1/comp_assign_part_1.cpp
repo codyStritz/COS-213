a8f35bf54b2842a43f26135aba7d49220492a11e
codyStritz <74121962+codyStritz@users.noreply.github.com>
2022-03-11 11:44:51 -0700

    cpa1 comments

--- cpa1/comp_assign_part_1.cpp
+++ cpa1/comp_assign_part_1.cpp
@@ -9,22 +9,25 @@
 #include <array>
 using namespace std;
 
+// Returns the first (alphabetical) letter of the two provided
+// based on ASCII value
 char FirstLetter (char x, char y) {
   return x < y ? x : y;
 }
 
+// Begins program execution
 int main()
 {
-  char first = 'z';
+  char first = 'z';   // Keeps track of current first letter
   char input;
-  for (int i = 0; i < 10; i++) {
-    //TODO: if user input invalid do i-- and break
+
+  for (int i = 0; i < 10; i++) {    // Loop 10 times
     if (i == 0) {
-      cout << "Please enter the first letter: ";
+      cout << "Please enter the first letter: ";  // Prompt user for input
     } else {
       cout << "Please enter the next letter: ";
     }
-    cin >> setw(1) >> input;
+    cin >> setw(1) >> input;    // Get user input
     // Check that input is alphabetical
     bool too_low = input < 'a';
     bool too_high = input > 'z';
@@ -33,11 +36,12 @@ int main()
       i--;
       continue;
     }
+    // Find first letter between first and input; update first
     first = FirstLetter(first, input);
   }
-  // Todo: add ASCII code
+  // Print first letter and ASCII code
   cout << "First letter: " << first << endl;
   cout << "ASCII value (decimal) of " << first << " is " << int(first) << endl;
-  system("pause");
+  system("pause");  // Pause command window
   return 0;
 }
