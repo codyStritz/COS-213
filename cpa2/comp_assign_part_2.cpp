a9c91ec1b01da5d2e09084d66759844412b45b4a
codyStritz <74121962+codyStritz@users.noreply.github.com>
2022-03-12 13:12:05 -0800

    cpa2 completed

--- cpa2/comp_assign_part_2.cpp
+++ cpa2/comp_assign_part_2.cpp
@@ -9,24 +9,28 @@
 #include <iomanip> 
 using namespace std;
 
-// Gets temperature in (F) from user and saves it to temp reference
+// Gets temperature from user and stores input at temp reference
 void getTemp(int& temp) {
   cout << "Please enter the temperature (F): ";
   cin >> setw(3) >> temp;
 }
 
+// Determines and prints activity based on temp
 void printActivity(int temp) {
-  string activity = "No Activity - Sending Everyone Home";
-  if (temp > 85) activity = "Swimming";
-  // else if (temp )
-
+  string activity = "No Activity - Sending Everyone Home";  // temp <= 32
+  if (temp > 85) activity = "Swimming";       // temp > 85
+  else if (temp > 70) activity = "Baseball";  // 70 < temp <= 85
+  else if (temp > 32) activity = "Basketball";  // 32 < temp <= 70
+  printf("The temperature for today of %d is considered to be a good"
+  " temperature for the following activity %s.\n", temp, activity.c_str());
 }
 
+// Begins program execution
 int main()
 {
-  // TODO: Implement main()
-  int temp;
-  getTemp(temp);
-  cout << temp << endl;
+  int temp;             // Declare temp variable
+  getTemp(temp);        // Get temp from user
+  printActivity(temp);  // Print activity based on temp
+  system("pause");      // Pause command window
   return 0;
 }
