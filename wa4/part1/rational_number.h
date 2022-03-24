2feee915e895ddf7e7d75d0d7a47a24d60cf5acb
codyStritz <74121962+codyStritz@users.noreply.github.com>
2022-03-24 00:09:45 -0700

    wa4 part 1

---
+++ wa4/part1/rational_number.h
@@ -0,0 +1,30 @@
+#include <string>
+using namespace std;
+// Prevent multiple inclusions of header
+#ifndef RATIONAL_NUMBER_H
+#define RATIONAL_NUMBER_H
+
+// RationalNumber class definition
+class RationalNumber {
+  public:
+    explicit RationalNumber(int = 1, int = 1); // default constructor
+    string toRationalString() const;
+    double toDouble() const;
+    RationalNumber operator+(RationalNumber& other);
+    RationalNumber operator-(RationalNumber& other);
+    RationalNumber operator*(RationalNumber& other);
+    RationalNumber operator/(RationalNumber& other);
+    bool operator<(RationalNumber& other);
+    bool operator>(RationalNumber& other);
+    bool operator<=(RationalNumber& other);
+    bool operator>=(RationalNumber& other);
+
+  private:
+    int numerator;
+    int denominator;
+    int getGCD(int, int);
+    int getLCM(int, int);
+};
+
+
+#endif //RATIONAL_NUMBER_H
